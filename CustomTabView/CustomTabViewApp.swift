95d4e4d73cd777cd755d29092f3074737101fe96
sanghee <sanghee.son@promr.co.kr>
2024-04-17 00:11:59 +0900

    Initial Commit

---
+++ CustomTabView/CustomTabViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabViewApp.swift
+//  CustomTabView
+//
+//  Created by break200 on 4/17/24.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
